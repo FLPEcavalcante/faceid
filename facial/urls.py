49fcef1bd2eb7278b9a50506277eef3009170bdc
Felipe Cavalcante <feilpe.cavalcante.hex360@gmail.com>
2023-01-03 18:34:25 -0300

    Rename url 'open-door/'

--- facial/urls.py
+++ facial/urls.py
@@ -1,9 +1,8 @@
-from django.urls import path, include
+from django.urls import path
+
 from . import views
 
 
 urlpatterns = [
-    # path('facial/', include('facial.urls'))
-    # path('open_door/', views.ListUsers.as_view()),
-    path('open_door_test/', views.door_test, name='door_test.facial'),
+    path('open-door/', views.OpenDoor.as_view(), name='open-door'),
 ]
