c99719de46b8c349d44a3a2a2f4a58f59257e3b0
Felipe Cavalcante <feilpe.cavalcante.hex360@gmail.com>
2023-01-03 18:33:01 -0300

    Adding Docstrings

--- facial/admin.py
+++ facial/admin.py
@@ -5,10 +5,33 @@ from . import models
 
 class AuthenticationAdmin(admin.ModelAdmin):
     """Auth model admin."""
+
     list_display = ('username', 'is_active',)
     list_filter = ('username', 'is_active', )
     search_fields = ('username', )
 
 
-admin.site.register(models.Authentication, AuthenticationAdmin)
+class DataRequestAdmin(admin.ModelAdmin):
+    """Data model admin."""
+
+    list_display = (
+        'id',
+        'matched',
+        'created_date',
+        'facial_creation_date',
+        'fullframe',
+    )
+
+    list_filter = (
+        'created_date',
+        'facial_creation_date'
+    )
+
+    readonly_fields = ('facial_creation_date',)
+
+    search_fields = ('id',)
+
+
 # Register your models here.
+admin.site.register(models.Authentication, AuthenticationAdmin)
+admin.site.register(models.DataRequest, DataRequestAdmin)
