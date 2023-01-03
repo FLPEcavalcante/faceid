d1b4a83a3dc7e61cbcc93285962ad9447ccdaf3c
Felipe Cavalcante <feilpe.cavalcante.hex360@gmail.com>
2023-01-03 18:35:03 -0300

    Adding class DataRequestSerializer

--- facial/serializers.py
+++ facial/serializers.py
@@ -1,8 +1,18 @@
 from rest_framework import serializers
-from .models import Authentication
+from .models import Authentication, DataRequest
 
 
 class AuthenticationSerializer(serializers.ModelSerializer):
+    """A class to serialize Authentication data."""
     class Meta:
+        """Meta class for AuthenticationSerializer."""
         model = Authentication
+        fields = '__all__'
+
+
+class DataRequestSerializer(serializers.ModelSerializer):
+    """A class to serialize Request Data."""
+    class Meta:
+        """Meta class for DataRequestSerializer."""
+        model = DataRequest
         fields = '__all__'
