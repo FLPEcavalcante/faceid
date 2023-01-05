96112927b952211ab218854f07fdc86883932776
Felipe Cavalcante <feilpe.cavalcante.hex360@gmail.com>
2023-01-05 18:00:02 -0300

    New methods file control_id/views.py

--- facial/control_id/views.py
+++ facial/control_id/views.py
@@ -3,36 +3,9 @@ import requests
 from facial import models, views
 
 
-class OpenDoorRequest():
-
-    def __init__(self, login_data, open_door_data):
-        self.login_data = login_data
-        self.open_door_data = open_door_data
-
-    auth = models.Authentication.objects.filter(is_active=True).first()
-
-    log = OpenDoorRequest(
-        login_data={
-            "login": auth.username,
-            "password": auth.password
-        }
-    )
-    print(log.login_data)
-
-    door = OpenDoorRequest(
-        open_door_data={
-            "actions": [
-                {
-                    "action": "sec_box",
-                    "parameters": "id=65793,reason=3,timeout=5000"
-                }
-            ]
-        }
-    )
-    print(door.open_door_data)
-
-    def authenticate(self):
-        """Method for validated authencication of login the user.
+# class OpenDoorRequest:
+def authenticate():
+    """Method for validated authencication of login the user.
 
         Args:
             login_data(JSON):
@@ -41,20 +14,35 @@ class OpenDoorRequest():
             value (session): return a value for session validated.
         """
 
-        login = requests.post(
-            settings.CONTROL_ID_URL_LOGIN, self.log
-        )
+    auth = models.Authentication.objects.filter(is_active=True).first()
 
-        if login.ok:
-            session_id = login.json().get('session')
-        else:
-            raise ValueError(f'Login attempt error: {login.json()}')
+    login_data = {
+        "login": auth.username,
+        "password": auth.password
+    }
+    import pdb
+    pdb.set_trace()
+    open_door_data = {
+        "actions": [
+            {
+                "action": "sec_box",
+                "parameters": "id=65793,reason=3,timeout=5000"
+            }
+        ]
+    }
 
-        open = requests.post(
-            f'{settings.CONTROL_ID_URL_DOOR}{session_id}', self.door
-        )
+    login = requests.post(settings.CONTROL_ID_URL_LOGIN, data=login_data)
 
-        if open.ok:
-            print(f'\n{settings.CONTROL_ID_URL_DOOR}{session_id}\n')
-        else:
-            raise ValueError(f'Login attempt error: {open.json(), open.url}')
+    if login.ok:
+        session_id = login.json().get('session')
+    else:
+        raise ValueError(f'Login attempt error: {login.json()}')
+
+    open = requests.post(
+        f'{settings.CONTROL_ID_URL_DOOR}{session_id}',
+        json=open_door_data)
+
+    if open.ok:
+        print(f'\n{settings.CONTROL_ID_URL_DOOR}{session_id}\n')
+    else:
+        raise ValueError(f'Login attempt error: {open.json(), open.url}')
--- facial/views.py
+++ facial/views.py
@@ -1,57 +1,10 @@
-import requests
-
-from rest_framework.generics import GenericAPIView
-from rest_framework.response import Response
+from django.http import JsonResponse
 from rest_framework import response, status, views
 from django.conf import settings
 
-from facial.control_id.views import OpenDoorRequest
+from facial.control_id.views import authenticate
+
 from facial.serializers import DataRequestSerializer
-
-from . import models
-
-
-# def authenticate():
-#     """Method for validated authencication of login the user.
-
-#     Args:
-#         login_data(JSON):
-
-#     Return:
-#         value (session): return a value for session validated.
-#     """
-
-#     auth = models.Authentication.objects.filter(is_active=True).first()
-
-#     login_data = {
-#         "login": auth.username,
-#         "password": auth.password
-#     }
-
-#     open_door_data = {
-#         "actions": [
-#             {
-#                 "action": "sec_box",
-#                 "parameters": "id=65793,reason=3,timeout=5000"
-#             }
-#         ]
-#     }
-
-#     login = requests.post(settings.CONTROL_ID_URL_LOGIN, data=login_data)
-
-#     if login.ok:
-#         session_id = login.json().get('session')
-#     else:
-#         raise ValueError(f'Login attempt error: {login.json()}')
-
-#     open = requests.post(
-#         f'{settings.CONTROL_ID_URL_DOOR}{session_id}',
-#         json=open_door_data)
-
-#     if open.ok:
-#         print(f'\n{settings.CONTROL_ID_URL_DOOR}{session_id}\n')
-#     else:
-#         raise ValueError(f'Login attempt error: {open.json(), open.url}')
 
 
 class OpenDoor(views.APIView):
@@ -67,8 +20,7 @@ class OpenDoor(views.APIView):
 
     def post(self, request):
 
-        OpenDoorRequest()
-        # authenticate()
+        authenticate()
         serializer = DataRequestSerializer(data=request.data, many=True)
         serializer.is_valid(raise_exception=True)
         serializer.save()
